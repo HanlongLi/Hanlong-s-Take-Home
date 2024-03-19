8543f3c80cef339c76bd116a84fdfd30fdc196fa
hli810 <hli810@gatech.edu>
2024-03-19 00:47:40 -0400

    Initial Commit

---
+++ FetchTakeHomeProj/FetchTakeHomeProjApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTakeHomeProjApp.swift
+//  FetchTakeHomeProj
+//
+//  Created by Lee on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchTakeHomeProjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
